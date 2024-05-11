d03d8f23ddb5b4be6c4fb23faae62bfdd4054c9c
hope2 <hope2u2us@gmail.com>
2024-05-11 18:49:16 -0400

    ext2

--- app/views_auto.py
+++ app/views_auto.py
@@ -9,7 +9,7 @@ from keras.utils import img_to_array
 from keras.models import load_model
 import cv2
 
-model_s = load_model("app/static/model/mnist_Adam2_20210516.h5")
+model_s = load_model("app/static/model/mnist_202311.h5")
 path = ""; secure_files = []
 	
 def allowed_file(filename):
